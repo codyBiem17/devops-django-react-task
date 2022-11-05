18f0ffea549cad20455a0d5b7d8f0542cbf3389f
codybiem17 <bellomaryam@ymail.com>
2022-11-05 23:29:40 +0100

    devops-django-react-task

---
+++ api/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/biem/Documents/CloudDevOps/HNG9/devops-django-react-task/api/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
